7d41bd1bae7a3007d5ee00b08fcfcf2f493886c5
JasonCMLo <jason.chun.man.lo@gmail.com>
2022-10-21 11:37:35 -0400

    Change to use mongo atlas

--- config/config.js
+++ config/config.js
@@ -1,2 +1,5 @@
 export const Secret = "MySecret";
-export const MongoURI  = "mongodb://127.0.0.1/media"
+// export const MongoURI  = "mongodb://127.0.0.1/media"
+
+export const MongoURI =
+  "mongodb+srv://JasonLo:Jason12345@cluster0.tstbsbq.mongodb.net/media?retryWrites=true&w=majority";
